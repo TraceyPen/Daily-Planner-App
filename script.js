78b9cf93c455e64da349bfd6d7a732503081c006
TraceyPen <traceylpender@yahoo.co.uk>
2024-01-21 18:46:54 +0000

    date showing and looping the times

---
+++ script.js
@@ -0,0 +1,27 @@
+dateDisplayEl = $("#currentDay");
+timeBlocks = $("#time-blocks")
+
+
+
+var today = dayjs().format('dddd[,] MMMM D');
+//console.log(today);
+dateDisplayEl.text(today)
+
+
+//HH = the hour in 2 digits
+var now = dayjs().format('HH');
+console.log(now)
+
+
+var times = ["9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00"];
+for (var i = 0; i < times.length; i++) {
+
+    timeBlocks.append(times[i]);
+
+
+
+
+}
+
+    
+  
