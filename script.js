71093e27e1ad9c051ed22bca780dc8f826f3beca
TraceyPen <traceylpender@yahoo.co.uk>
2024-01-22 18:11:44 +0000

    classes past present or future added based on time now

--- script.js
+++ script.js
@@ -11,13 +11,33 @@ dateDisplayEl.text(today)
 //HH = the hour in 2 digits
 var now = dayjs().format('HH');
 console.log(now)
+console.log(typeof now)
+numberNow = now * 1;
+console.log(numberNow);
+console.log(typeof numberNow);
 
 
 
 var times = ["9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00"];
-for (var i = 0; i < times.length; i++) {
+var hours = ["9", "10", "11", "12", "13", "14", "15", "16", "17", "18"];
 
+
+for (var i = 0; i < times.length; i++) {
+//create list item
     var timesEl = $('<li>'); 
+
+    //compare the time now to the time on the calendar to allocate class past/present/future
+ if (numberNow == hours[i]) {
+        timesEl.attr('class', 'present');
+    }
+    else if (hours[i] < numberNow){
+        timesEl.attr('class', 'past');
+    
+    }
+    else {
+    times.El.attr('class', 'future');
+    }
+
     timesEl.text(times[i]);
 
     timesEl.addClass('row');
@@ -25,6 +45,9 @@ for (var i = 0; i < times.length; i++) {
     timeBlocksEl.append(timesEl);
         
 }
+
+
+
 
 
 
